bb621a20e95d8dd83082fb955e4a1eab2fc84ca4
Anjali-abc <90022797+Anjali-abc@users.noreply.github.com>
2021-11-01 12:54:23 +0530

    Add files via upload

---
+++ xscript.js
@@ -0,0 +1,22 @@
+var  x;
+console.log(x);
+
+if(x==undefined)
+{
+	console.log("x is undefined");
+}
+
+var y = 5;
+if(y==undefined)
+{
+	console.log("y is undefined");
+}
+else{
+	console.log("y is defined");
+}
+
+console.log(z);
+if(z==undefined)
+{
+	console.log("z is undefined")
+}
